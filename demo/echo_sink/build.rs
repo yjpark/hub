96b6720de71fb9261f17f962731cee2ff34c3b77
YJ Park <yjpark@gmail.com>
2023-03-21 15:42:06 +0800

    cleanup grpc hub codes, plan to work on development server first

--- demo/echo_sink/build.rs
+++ demo/echo_sink/build.rs
@@ -1,10 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../../proto/google/rpc/error_details.proto")?;
     tonic_build::configure()
         .build_server(false)
-        .build_client(true)
+        .build_client(false)
         //.type_attribute("edger.hub.v1.link.AppRequest", "#[derive(serde::Serialize)]")
-        .compile(&["../../proto/v1/sink.proto"], &["../../proto/v1"])
+        .compile(&["../proto/v1/echo.proto"], &["../proto/v1"])
         .unwrap();
     Ok(())
 }
--- grpc/sink_hub/build.rs
+++ grpc/sink_hub/build.rs
@@ -1,10 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .build_server(false)
-        .build_client(false)
         //.type_attribute("edger.hub.v1.link.AppRequest", "#[derive(serde::Serialize)]")
-        .compile(&["../../proto/v1/echo.proto"], &["../../proto/v1"])
+        .compile(&["../proto/v1/sink.proto"], &["../proto/v1"])
         .unwrap();
     Ok(())
 }
-
