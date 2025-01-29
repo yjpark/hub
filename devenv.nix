5b65237929a5d2b6bcd465256f8c79c98acf841f
YJ Park <yjpark@gmail.com>
2025-01-29 13:33:30 +0800

    🚧 [WIP] - 2025-01-29 13:33:30 -

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,9 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = with pkgs ; [
+    git
+  ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
